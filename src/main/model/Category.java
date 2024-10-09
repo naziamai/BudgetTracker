3a547f339b4316f3b31ab64451afa06f04960a02
nchowd09 <naziamaisha@gmail.com>
2024-10-08 20:07:16 -0700

    Added specification for category class

---
+++ src/main/model/Category.java
@@ -0,0 +1,24 @@
+package model;
+import java.util.List;
+
+public class Category {
+    String name;
+    int limit;
+    List<Double> listOfExpense;
+
+    public Category(String name) {
+       this.name = name;
+    }
+
+    //MODIFIES: this 
+    //EFFECTS: sets limit for the category 
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+
+    //MODIFIES: this 
+    //EFFECTS: Adds a single expense to the list of expenses 
+    public void addExpense() {
+
+    }
+}
