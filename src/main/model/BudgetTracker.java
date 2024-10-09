82a6b4c2cea59b1dd219649c16a347244263b6a2
nchowd09 <naziamaisha@gmail.com>
2024-10-08 21:22:05 -0700

    Added specification for the BudgetTracker Class

--- src/main/model/BudgetTracker.java
+++ src/main/model/BudgetTracker.java
@@ -1,5 +1,31 @@
 package model;
+import java.util.List;
 
 public class BudgetTracker {
+    List<Category> listOfCategory;
+    int totalBudget;
+
+    public BudgetTracker(){
+        //stub
+    }
+    //MODIFIES: this
+    //EFFECT: Adds a new category to the list of categories
+    public void addCategory(Category category){
+
+    }
+    //MODIFIES: this
+    //EFFECT: Adds a new category to the list of categories
+    public void removeCategory(Category category){
+
+    }
+    //MODIFIES: this
+    //EFFECTS: Calculates the total budget added across all categories 
+    public void calculateBudget(){
+        
+    }
+    // EFFECTS: Returns the total budget 
+    public int getTotalBudget(){
+        return 0;
+    }
 
 }
