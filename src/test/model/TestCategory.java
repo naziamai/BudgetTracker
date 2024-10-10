a5c484540377e187907765d421097da625ff7a5d
nchowd09 <naziamaisha@gmail.com>
2024-10-09 19:50:52 -0700

    Added tests for Category class

---
+++ src/test/model/TestCategory.java
@@ -0,0 +1,42 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestCategory {
+    Category testCategory;
+    List<Double> testListOfExpense;
+    
+    @BeforeEach
+    void runBefore() {
+        testCategory = new Category("Grocery", 300.00);
+
+    }
+
+    @Test 
+    void testContructor() {
+        assertEquals("Grocery", testCategory.getName());
+        assertEquals(300.00, testCategory.getLimit());
+
+    }
+
+    @Test
+    void testAddSingleExpense() {
+        testCategory.addExpense(50.00);
+        List<Double> expenseAdded = new ArrayList<>();
+        assertEquals(expenseAdded.add(50.00), testCategory.getListOfExpense());
+    }
+
+    @Test 
+    void testAddMultipleExpense() {
+        testCategory.addExpense(60.00);
+        testCategory.addExpense(75.10);
+        List<Double> expenseAdded = new ArrayList<>();
+        assertEquals(expenseAdded, testCategory.getListOfExpense());
+    }
+}
