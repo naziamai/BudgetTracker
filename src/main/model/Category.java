aed6eccba50396b99da27f8d3dd8e0d455d810a9
nchowd09 <naziamaisha@gmail.com>
2024-10-09 19:44:59 -0700

    Updated specification for Category class

--- src/main/model/Category.java
+++ src/main/model/Category.java
@@ -3,22 +3,40 @@ import java.util.List;
 
 public class Category {
     String name;
-    int limit;
+    Double limit;
     List<Double> listOfExpense;
 
-    public Category(String name) {
-       this.name = name;
+    public Category(String name, Double limit) {
+       //stub
     }
 
+    //REQUIRES: limit > 0.00
     //MODIFIES: this 
     //EFFECTS: sets limit for the category 
-    public void setLimit(int limit) {
-        this.limit = limit;
+    public void setLimit(Double limit) {
+        
     }
-
+    //REQUIRES: expense > 0.00
     //MODIFIES: this 
     //EFFECTS: Adds a single expense to the list of expenses 
-    public void addExpense(int expense) {
+    public void addExpense(Double expense) {
 
+    }
+    // EFFECTS: Returns the remaining limit on the category
+    public Double getRemainingLimit() {
+        return 0.00;
+    }
+    //EFFECTS: returns list of expenses
+    public List<Double> getListOfExpense() {
+        return null;
+    }
+
+    //EFFECTS: Returns the name of the category 
+    public String getName(){
+        return name;
+    }
+    //EFFECTS: Returns the limit of the category 
+    public Double getLimit() {
+        return 0.00;
     }
 }
