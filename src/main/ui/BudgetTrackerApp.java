dbbe3e9d7dd15fd79a89b149a0068f1197566aef
nchowd09 <naziamaisha@gmail.com>
2024-10-10 22:04:27 -0700

    Added Specification on the BudgetTrackerApp

---
+++ src/main/ui/BudgetTrackerApp.java
@@ -0,0 +1,42 @@
+package ui;
+
+import java.util.*;
+
+import model.BudgetTracker;
+import model.Category;
+import model.Report;
+
+public class BudgetTrackerApp {
+    private List<Category> Categories;
+    private Report report;
+    private BudgetTracker budgetTracker;
+    private Scanner input;
+
+    //EFFECTS: runs the budget tracker app
+    public BudgetTrackerApp() {
+    }
+
+    //MODIFIES: this
+    //EFFECTS: processes user input
+    public void runApp() {
+
+    }
+    //MODIFIES: this
+    //EFFECTS: processes user input 
+    public void executeCommand() {
+
+    }
+    //MODIFIES: this
+    //EFFECTS: display the menu 
+    public  void displayMenu(){
+
+    }
+
+    //MODIFIES: this 
+    //EFFECTS: Continue running the program until the user selects quit option 
+    public void continueRunning() {
+
+    }
+
+
+}
