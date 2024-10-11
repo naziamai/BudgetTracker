dfcc38075791ec96ea69bfb117e94cc095646214
nchowd09 <naziamaisha@gmail.com>
2024-10-10 18:21:01 -0700

    Added Specification for the Report Class

--- src/main/model/Report.java
+++ src/main/model/Report.java
@@ -1,5 +1,24 @@
 package model;
 
-public class Report {
+import java.time.LocalDate;
+import java.util.List;
 
+public class Report {
+    private List<Category> listOfCategory;
+    private List<LocalDate> listOfDates;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Report(List<Category> listOfCategories, LocalDate startDate, LocalDate endDate){
+         
+    }
+    
+
+    //REQUIRES: Valid start and end date entered 
+    //EFFECTS: If a date in list of date is between the start and end date then that expense will generated in summary
+    //         in the form Category: Expense
+      
+    public void generateSummary(LocalDate startDate, LocalDate endDate){
+
+    }
 }
