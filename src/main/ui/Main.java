10a0369d7f3b29d8755f7461cecd2bd832a90f83
nchowd09 <naziamaisha@gmail.com>
2024-11-22 00:37:48 -0800

    Main implementation

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,8 +1,28 @@
 package ui;
 
+import javax.swing.*;
+
+import model.BudgetTracker;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        BudgetTrackerApp budgetTrackerApp = new BudgetTrackerApp();
-        budgetTrackerApp.runApp();
+        System.out.println("Choose application mode:");
+        System.out.println("1. Console");
+        System.out.println("2. GUI");
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        if (choice == 1) {
+            BudgetTrackerApp budgetTrackerApp = new BudgetTrackerApp();
+            budgetTrackerApp.runApp();
+        } else if (choice == 2) {
+            BudgetTracker budgetTracker = new BudgetTracker();
+            new GUI(budgetTracker);
+        } else {
+            System.out.println("Invalid choice. Exiting application.");
+        }
     }
 }
