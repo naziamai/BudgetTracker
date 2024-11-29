c6131c0b0326db5514958509e0767da03cb69097
nchowd09 <naziamaisha@gmail.com>
2024-11-29 12:56:51 -0800

    Fixed code style errors

--- src/test/model/TestEvent.java
+++ src/test/model/TestEvent.java
@@ -13,28 +13,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * Unit tests for the Event class
  */
 public class TestEvent {
-	private Event e;
-	private Date d;
+    private Event event;
+    private Date date;
 	
 	//NOTE: these tests might fail if time at which line (2) below is executed
 	//is different from time that line (1) is executed.  Lines (1) and (2) must
 	//run in same millisecond for this test to make sense and pass.
 	
-	@BeforeEach
-	public void runBefore() {
-		e = new Event("Sensor open at door");   // (1)
-		d = Calendar.getInstance().getTime();   // (2)
-	}
+    @BeforeEach
+    public void runBefore() {
+        event = new Event("Sensor open at door");   // (1)
+        date = Calendar.getInstance().getTime();   // (2)
+    }
 	
-	@Test
+    @Test
 	public void testEvent() {
-		assertEquals("Sensor open at door", e.getDescription());
-		assertEquals(d, e.getDate());
-	}
+        assertEquals("Sensor open at door", event.getDescription());
+        assertEquals(date, event.getDate());
+    }
 
-	@Test
+    @Test
 	public void testToString() {
-		assertEquals(d.toString() + "\n" + "Sensor open at door", e.toString());
-	}
+        assertEquals(date.toString() + "\n" + "Sensor open at door", event.toString());
+    }
 }
 
